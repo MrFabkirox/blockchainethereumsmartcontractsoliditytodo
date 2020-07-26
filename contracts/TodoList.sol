b94f13dee0cea4d8e1d3f1a08cd79017f99e4a23
Fabrice <fesope@ironflytechnologies.com>
2020-07-26 14:31:55 +0800

    need toggle test fixing

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,22 +1,41 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-    uint256 public taskCount = 0;
+  uint256 public taskCount = 0;
 
-    struct Task {
-        uint256 id;
-        string content;
-        bool completed;
-    }
+  struct Task {
+      uint256 id;
+      string content;
+      bool completed;
+  }
 
-    mapping(uint256 => Task) public tasks;
+  mapping(uint => Task) public tasks;
 
-    constructor() public {
-        createTask("check out dappuniversity.com");
-    }
+  event TaskCreated(
+    uint id,
+    string content,
+    bool completed
+  );
 
-    function createTask(string memory _content) public {
-        taskCount++;
-        tasks[taskCount] = Task(taskCount, _content, false);
-    }
+  constructor() public {
+      createTask("Check out dappuniversity.com");
+  }
+
+  event TaskCompleted(
+    uit id,
+    bool completed
+  )
+
+  function createTask(string memory _content) public {
+    taskCount++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+    emit TaskCreated(taskCount, _content, false);
+  }
+
+  function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.comleted = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(id, _task.completed);
+  }
 }
